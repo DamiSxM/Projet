7db8d37c8a3e060ed79d612d26b4631e22ed5acd
Dami <dami.bachelet@gmail.com>
2017-06-14 12:19:49 +0200

    Création du projet Visual Studio
    Création de la classe Salarie avec les attributs

---
+++ ProjetGitHub/ProjetGitHub/Salarie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetGitHub
+{
+    public class Salarie
+    {
+        #region Attributs
+        string _matricule = string.Empty;
+        string _nom = string.Empty;
+        string _prenom = string.Empty;
+        decimal _salaireBrut = 0;
+        decimal _tauxCS = 0;
+        DateTime _dateNaissance;
+        #endregion
+    }
+}
