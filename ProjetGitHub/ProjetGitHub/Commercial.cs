e0de5f5742c0ce152499b2761aa4451a8e001e6d
Dami <dami.bachelet@gmail.com>
2017-06-14 13:29:09 +0200

    Ajout de la classe Commercial
    Dans un nouvelle branche Commercial

---
+++ ProjetGitHub/ProjetGitHub/Commercial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetGitHub
+{
+    class Commercial : Salarie
+    {
+        #region Attributs
+        decimal _chiffreAffaire;
+        decimal _commission;
+        #endregion
+    }
+}
